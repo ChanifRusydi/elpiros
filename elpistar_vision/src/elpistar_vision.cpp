eabdf6c186191999b0501e7a6ce6452df28055d1
elpistars <elpistars@gmail.com>
2020-01-20 23:34:24 +0700

    vision cropped

--- elpistar_vision/src/elpistar_vision.cpp
+++ elpistar_vision/src/elpistar_vision.cpp
@@ -18,74 +18,92 @@ int main(int argc, char* argv[])
 {
 
 
-ros::init(argc, argv,"line_node");
-ros::NodeHandle nh;
-image_transport::ImageTransport it(nh);
-image_transport::Publisher image_pub = it.advertise("test_cam/image_raw",1);
-sensor_msgs::ImagePtr ros_img;
+	ros::init(argc, argv,"line_node");
+	ros::NodeHandle nh;
+	image_transport::ImageTransport it(nh);
+	image_transport::Publisher image_pub = it.advertise("test_cam/image_raw",1);
+	sensor_msgs::ImagePtr ros_img;
 	VideoCapture cap(0);
-	cap.set(3, 320.0);
-	cap.set(4, 240.0);
+	cap.set(3, 160.0);
+	cap.set(4, 120.0);
+	cap.set(CAP_PROP_FOURCC, CV_FOURCC('M', 'J', 'P', 'G'));
 
 	while (ros::ok()) {
-		Mat frame, gray, blur, th;
+		Mat frame, crop, gray, blur, th;
+		Rect roi;
 		cap.read(frame);
-		cvtColor(frame, gray, COLOR_BGR2GRAY);
+
+		int offset_x = 0;
+		int offset_y = 60;
+		roi.x = offset_x;
+		roi.y = offset_y;
+		roi.width = frame.size().width - (offset_x * 2);
+		roi.height = frame.size().height - (offset_y * 2);
+
+		crop = frame(roi);
+
+		cvtColor(crop, gray, COLOR_BGR2GRAY);
 		GaussianBlur(gray, blur, Size(5, 5), 0);
-		threshold(blur, th, 60, 255, THRESH_BINARY + THRESH_OTSU);
+		threshold(blur, th, 60, 255, THRESH_BINARY_INV);
 		
 		vector<vector<Point>> contours;
 		vector<Vec4i> hierarchy;
-		findContours(th, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+		findContours(th, contours, hierarchy, RETR_TREE, CHAIN_APPROX_NONE);
 
 		Scalar color = Scalar(0, 255, 0);
-		drawContours(frame, contours, -1, color, 3);
+		drawContours(crop, contours, -1, color, 3);
 
 
 	//	imshow("Image", frame);
 
 
 		Moments M;
-		int cx, cy,len;
+		int cx, cy;
 		string status;
-                 len =  1;
-		//for (int i = 0; i <= contours.size() - 1; i++) {
-			//double area = contourArea(contours[i]);
-			if (contours.size() >= 0) {
-				M = moments(contours[i]);
+
+
+		if (contours.size() > 0) {
+			int c=0;
+			for (int i = 1; i <= contours.size() - 1; i++) {
+				if( contourArea(contours[c]) < contourArea(contours[i]) ){
+					c=i;
+				}
+			}
+
+			M = moments(contours[c]);
+			if (M.m00 != 0) {
 				cx = (int)(M.m10 / M.m00);
 				cy = (int)(M.m01 / M.m00);
-                              line(frame, Point(cx, 0), Point(cx, 240), Scalar(255,0,0), 2, len);
-                              line(frame, Point(0, cy), Point(320, cy), Scalar(255,0,0), 2, len);
-                              drawContours(frame, contours, -1,Scalar(0,255,0),len);
-				if (cx >= 120) {
-					status = "Kiri";
-				}
-				if (cx < 120  && cx > 50) {
-					status = "Kanan";
-				}
-				if(cx <= 50) {
-					status = "Tengah";
-				}
-                            
-				cout << "Centroid X: " << cx;
-				cout << " Centroid Y: " << cy;
-				cout << " Status: " << status << endl;
-			}      
-
-
-	// 	}
-
+			} else {
+				cx = (int)(M.m10 / 1);
+				cy = (int)(M.m01 / 1);
+			}
+			line(crop, Point(cx, 0), Point(cx, 240), Scalar(255,0,0), 2);
+			line(crop, Point(0, cy), Point(320, cy), Scalar(255,0,0), 2);
+			drawContours(crop, contours, -1,Scalar(0,255,0));
+			if (cx >= 120) {
+				status = "Kiri";
+			}
+			if (cx < 120  && cx > 50) {
+				status = "Tengah";
+			}
+			if(cx <= 50) {
+				status = "Kanan";
+			}
+						
+			cout << "Centroid X: " << cx;
+			cout << " Centroid Y: " << cy;
+			cout << " Status: " << status << endl;
+		}      
+	
 		if (waitKey(30) == 27) {
 			cout << "Key has been pressed" << endl;
 			break;
 		}
 
- ros_img=cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
-  image_pub.publish(ros_img);
- ros::spinOnce();
-
-
+		ros_img=cv_bridge::CvImage(std_msgs::Header(), "bgr8", crop).toImageMsg();
+		image_pub.publish(ros_img);
+		ros::spinOnce();
 	}
 }
 
